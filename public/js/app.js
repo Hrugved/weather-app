189a705df4bd662a295043c8546ade46298cc5d3
Hrugved <pineappleiitian@gmail.com>
2019-08-16 21:06:56 +0530

    Complete geocoding marker, styling left

--- public/js/app.js
+++ public/js/app.js
@@ -7,11 +7,18 @@ const map = new mapboxgl.Map({
     zoom: 5
 })
 
-const weatherForm = document.querySelector('.search')
-const search = document.querySelector('.search__input')
+var marker = new mapboxgl.Marker({
+    draggable: true
+    })
+    .setLngLat([-73.99462890625558,40.74491110332883])
+    .addTo(map);
+
+const weatherForm = document.querySelector('.form')
+const search = document.querySelector('.form__input')
 // const forecast = document.querySelector('.forecast')
 const summary = document.querySelector('.forecast__summary')
-const tempCurr = document.querySelector('.forecast__temp-curr')
+const tempCurrText = document.querySelector('.forecast__temp-curr__text')
+const tempCurrValue = document.querySelector('.forecast__temp-curr__value')
 const tempHigh = document.querySelector('.forecast__temp-high')
 const tempLow= document.querySelector('.forecast__temp-low')
 const rainProb = document.querySelector('.forecast__rain-prob')
@@ -19,38 +26,45 @@ const rainProb = document.querySelector('.forecast__rain-prob')
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
-    // messageOne.textContent = 'fetching the weather...'
-    // summary.textContent = ''
-
     const location = search.value
 
     const url = '/weather?address=' + location
 
     console.log('fetching url ' + url)
 
+    fetchAndDisplay(url)
+})
+
+marker.on('dragend',() => {
+    const coords = marker.getLngLat()
+    console.log(coords)
+    search.value = ''
+    const url = '../forecast?lng=' + coords.lng + '&lat=' + coords.lat
+    console.log(url)
+    fetchAndDisplay(url)
+})
+
+fetchAndDisplay = (url) => {
     fetch(url).then((response) => {
-        response.json().then((data) => {
-            if(data.error) {
-                return console.log('Fetching error!')
-            } 
-            console.log(data)
-            
-            summary.textContent = data.forecast.summary + '.'
-            tempCurr.textContent = 'Current temperature is ' + data.forecast.temp__curr
-            tempHigh.textContent = 'with high of ' + data.forecast.temp__high
-            tempLow.textContent = 'and low of ' + data.forecast.temp__low
-            rainProb.textContent = 'There is ' + data.forecast.rain__prob + ' % chance of rain'
+    response.json().then((data) => {
+        if(data.error) {
+            return console.log('Fetching error!')
+        } 
+        console.log(data)
+        
+        summary.textContent = data.forecast.summary + '.'
+        tempCurrText.textContent = 'Current temp of ' 
+        tempCurrValue.textContent = data.forecast.temp__curr
+        tempHigh.textContent = 'with high of ' + data.forecast.temp__high
+        tempLow.textContent = 'and low of ' + data.forecast.temp__low
+        rainProb.textContent = 'With ' + data.forecast.rain__prob + ' % chance of rain'
 
-            map.flyTo({
-                center: [data.longitude,data.latitude],
-                zoom:10
-            })
+        map.flyTo({
+            center: [data.longitude,data.latitude],
+            zoom:10
+        })
 
-            var marker = new mapboxgl.Marker({
-                draggable: true
-                })
-                .setLngLat([data.longitude, data.latitude])
-                .addTo(map);
-        })    
+        marker.setLngLat([data.longitude, data.latitude]).addTo(map);
     })
 })
+}
--- src/app.js
+++ src/app.js
@@ -33,24 +33,34 @@ app.get('/weather',(req,res) => {
                 error: err
             })
         }
-        // res.send({
-        //     longitude,
-        //     latitude
-        // })
-        forecast(latitude,longitude, (err,forecastData) => {
-            if(err) {
-                return res.send({
-                    error: err
-                })
-            }
 
-            res.send({
-                address: req.query.address,
-                forecast: forecastData,
-                location,
-                latitude,
-                longitude
+        const url = '../forecast?lng=' + longitude + '&lat=' + latitude
+
+        return res.redirect(url)
+    })
+})
+
+app.get('/forecast', (req,res) => {
+    // CHECK : possible problem if lng or lat = 0
+    if(!req.query.lng || !req.query.lat) {
+        return res.send({
+            error: 'Invalid coordinates recieved'
+        })
+    }
+    const longitude = req.query.lng
+    const latitude = req.query.lat
+
+    forecast(latitude,longitude, (err,forecastData) => {
+        if(err) {
+            return res.send({
+                error: err
             })
+        }
+
+        res.send({
+            forecast: forecastData,
+            latitude,
+            longitude
         })
     })
 })
